52fd4b3febd01454bbbe3865944cdd210cdc7a99
zattri <daninjakidy@gmail.com>
2020-12-27 00:17:46 +0000

    Cleaned up code a bit, trying to separate out section into other files, not sure where to separate though

--- data_mining/coursework/preprocessing.R
+++ data_mining/coursework/preprocessing.R
@@ -3,52 +3,49 @@ library("dbscan")
 setwd("../Desktop/masters/data_mining/coursework")
 
 dataset = read.csv("regional_covid_data.csv")
-head(dataset)
-str(dataset)
 
-# Drop areaType and areaCode columns, these are unnecessary
+# Removing unnecessary columns and renaming
 dataset = subset(dataset, select = -c(areaType, areaCode))
+names(dataset)[names(dataset) == "cumCasesBySpecimenDate"] = "cumCases"
+names(dataset)[names(dataset) == "newCasesBySpecimenDate"] = "newCases"
+names(dataset)[names(dataset) == "cumDeathsByDeathDate"] = "cumDeaths"
+names(dataset)[names(dataset) == "newDeathsByDeathDate"] = "newDeaths"
+names(dataset)[names(dataset) == "newCasesBySpecimenDateRollingRate"] = "newCasesAvg" 
+# Rolling rate is a calculated weekly average
 
-# Remove null values from the dataset
+# Remove null values
 dataset = dataset[rowSums(is.na(dataset)) == 0,]
-
-# Convert dataset areaNames to numerical values - Label encoding
-# Get unique area names into vector
+# Label encode regions as integers
 regions = unique(dataset$areaName)
-
-#Convert to numeric values and set dataset$areaNames to numeric values
 dataset$areaName = as.numeric(factor(dataset$areaName, levels=regions))
 
-# Convert dates from chr format to valid date format
+
+# Convert dates to valid date format and select date range
 dataset$date = as.Date(dataset$date, "%d/%m/%Y")
-
-
-# Select records within specific range
+# Time-span cut-off
 dataset = dataset[dataset$date >= as.Date("2020-04-01"),] # Beginning of dataset
-dataset = dataset[dataset$date >= as.Date("2020-09-01"),] # Recent lockdown selection
+dataset = dataset[dataset$date >= as.Date("2020-09-01"),] # 2nd Lockdown released
+
+write.csv(dataset, "visualisation_dataset.csv")
 
 
-# Convert the dates to ordered integer of days since first selected record in dataset
+#Standardisation
 dataset$date = as.numeric(dataset$date - min(dataset$date, na.rm = TRUE))
-
-# Standardise date numbers to be between 0 and 1
 dataset$stdDate = dataset$date - min(dataset$date, na.rm = TRUE)
 dataset$stdDate = dataset$date / max(dataset$date, na.rm = TRUE)
-# Standardise the rolling average
-dataset$stdRollRate = dataset$newCasesBySpecimenDateRollingRate - min(dataset$newCasesBySpecimenDateRollingRate, na.rm = TRUE)
-dataset$stdRollRate = dataset$newCasesBySpecimenDateRollingRate / max(dataset$newCasesBySpecimenDateRollingRate, na.rm = TRUE)
+dataset$stdRollRate = dataset$newCasesAvg - min(dataset$newCasesAvg, na.rm = TRUE)
+dataset$stdRollRate = dataset$newCasesAvg / max(dataset$newCasesAvg, na.rm = TRUE)
 
 
-
-
+# DBScan pre-processing
 dataMatrix = cbind(dataset$stdDate, dataset$stdRollRate)
-nonStdDataMatrix = cbind(dataset$date, dataset$newCasesBySpecimenDateRollingRate)
+nonStdDataMatrix = cbind(dataset$date, dataset$newCasesAvg)
 colnames(dataMatrix) = c("Date", "RollRate")
 colnames(nonStdDataMatrix) = c("Date", "RollRate")
-#plot(dataMatrix)
 
+# Finding the optimal epoc for minimum points
 kNNdistplot(dataMatrix, k=2)
-kNNdistplot(dataMatrix, k=3)
+kNNdistplot(dataMatrix, k=3) # Min points 3 seems best for  Sept+ data
 kNNdistplot(dataMatrix, k=4)
 kNNdistplot(dataMatrix, k=5)
 abline(h = 0.04, lty = 2)
@@ -63,6 +60,7 @@ kNNdistplot(dataMatrix, k=9)
 kNNdistplot(dataMatrix, k=10)
 kNNdistplot(dataMatrix, k=15)
 kNNdistplot(dataMatrix, k=20)
+
 
 # DBScan
 dbscanOut = dbscan(dataMatrix, eps=0.02, MinPts = 3) # The best one for Sept data
@@ -80,35 +78,21 @@ plot(dbscanOut, nonStdDataMatrix)
 
 
 # Chart analysis
-westMid = dataset[dataset$areaName == 5,]
-plot(westMid$date, westMid$newCasesBySpecimenDateRollingRate, xlab="Date", ylab="New Cases, Rolling Rate", pch=19)
-plot(westMid$date, westMid$newCasesBySpecimenDate, xlab="Date", ylab="New Cases, Rolling Rate", pch=19)
 
-septStats = dataset[(dataset$areaName == 5 || dataset$areaName ==  8) & dataset$date >= as.Date("2020-09-01"),]
-plot(westMidRecent$date, westMidRecent$newCasesBySpecimenDate, xlab="Date", ylab="New Cases", pch=19, main="COVID-19 West Midlands new cases per day since Sept")
+# New cases per day
+plot(dataset$date, dataset$newCases, col=dataset$areaName, xlab="Date", ylab="New Cases", pch=19, 
+     main="COVID-19, New cases per day")
+legend("topleft", legend=regions, col=c(1:9), pch=19)
 
-southEastRecent = dataset[dataset$areaName == 8 & dataset$date >= as.Date("2020-09-01"),]
+# Rolling Rate Chart
+plot(dataset$date, dataset$newCasesAvg, col=dataset$areaName, xlab="Date", ylab="New Cases", pch=19, 
+     main="COVID-19, Average cases per week")
+legend("topleft", legend=regions, col=c(1:9), pch=19)
 
-septStats = dataset[(dataset$areaName == 5 | dataset$areaName ==  8) & dataset$date >= as.Date("2020-09-01"),]
-plot(septStats$date, septStats$newCasesBySpecimenDate, col=septStats$areaName, xlab="Date", ylab="New Cases", pch=19, 
-    main="COVID-19, New cases per day since Sept")
-legend("topleft", legend=c("West Mid", "South"), col=c(5,8), pch=19)
-
-regionStats = dataset[dataset$date >= as.Date("2020-11-01"),]
-plot(regionStats$date, regionStats$newCasesBySpecimenDate, col=regionStats$areaName, xlab="Date", ylab="New Cases", pch=19, 
-     main="COVID-19, New cases per day since Sept")
-legend("topleft", legend=regions[1:9], col=c(1:9), pch=19)
-
-regionStats = dataset[dataset$date >= as.Date("2020-09-01"),] #& dataset$areaName <= 8
-plot(regionStats$date, regionStats$newCasesBySpecimenDateRollingRate, col=regionStats$areaName, xlab="Date", ylab="New Cases", pch=19, 
-     main="COVID-19, Average cases per week since September")
-legend("topleft", legend=regions[1:9], col=c(1:9), pch=19)
-
-# Cummulative Deaths Graph
-dataset = dataset[dataset$date >= as.Date("2020-10-01"),]
-plot(dataset$date, dataset$newDeathsByDeathDate, col=dataset$areaName, xlab="Date", ylab="Deaths per 100k", pch=19, 
-     main="COVID-19, Cummulative Deaths")
-legend("topleft", legend=regions[1:9], col=c(1:9), pch=19)
+# Cumulative Deaths Chart
+plot(dataset$date, dataset$newDeaths, col=dataset$areaName, xlab="Date", ylab="Deaths per 100k", pch=19, 
+     main="COVID-19, Cummulative deaths")
+legend("topleft", legend=regions, col=c(1:9), pch=19)
 
 
 
